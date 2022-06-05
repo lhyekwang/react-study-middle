8bf2e2247bce4ebba57e7182d436d75f0c48cada
bean.zino <bean.zino@kakaocorp.com>
2022-06-05 18:35:14 +0900

    SPA 초기로딩처리하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 import Search from './search/container/Search';
 import User from './user/container/User';
 import { Route } from 'react-router-dom';
@@ -6,6 +6,11 @@ import { Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
 export default function App() {
+  useEffect(() => {
+    const bodyEl = document.getElementsByTagName('body')[0];
+    const loadingEl = document.getElementById('init-loading');
+    bodyEl.removeChild(loadingEl);
+  }, []);
   return (
     <>
       <Route exact path="/" component={Search} />
